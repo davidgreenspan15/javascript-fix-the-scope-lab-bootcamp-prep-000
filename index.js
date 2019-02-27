9861fa130d03f9fd60e4b73d0f3c4b919dc711e9
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-02-27 16:34:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ return funky
 }
 function funkyFunction() {
  return theFunk()
-     
+var theFunk = funkyFunction()     
   }
 
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+
